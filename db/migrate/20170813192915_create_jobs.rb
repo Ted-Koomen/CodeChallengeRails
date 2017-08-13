d1156b93f0d52b1cb2ec031dbc531b947b139f16
Ted-Koomen <koomen.ted@gmail.com>
2017-08-13 15:33:16 -0400

    add jobs controller

---
+++ db/migrate/20170813192915_create_jobs.rb
@@ -0,0 +1,12 @@
+class CreateJobs < ActiveRecord::Migration[5.1]
+  def change
+    create_table :jobs do |t|
+      t.text :title
+      t.text :description
+      t.boolean :urgent
+      t.datetime :date_completed
+
+      t.timestamps
+    end
+  end
+end
