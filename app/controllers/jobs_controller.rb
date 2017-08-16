55604507f743619015544225eaba2356d59b0c18
Ted-Koomen <koomen.ted@gmail.com>
2017-08-16 13:14:00 -0400

    fix typos

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,37 @@
 class JobsController < ApplicationController
+
   def index
-    jobs = Job.all
-    render json:jobs
+    render json:Job.all
   end
+
+  def create
+    job = Job.new
+    job.title = params[:job][:title]
+    job.description = params[:job][:description]
+    if job.save
+      render json: job
+    else
+      render "Save Unsuccessful"
+    end
+  end
+
+  # def new
+  #   job = Job.new
+  #   job.title = params[:content]
+  #   job.description = params[:description]
+  #   job.importance = params[:urgent]
+  #   job.date_completed = params[:date_completed]
+  #   if job.save
+  #     render json: job
+  #   else
+  #     render "Save unsucessful"
+  #   end
+  # end
+
+  private
+
+  def job_params
+    params.require(:job).permit( :title, :description, :urgent, :date_completed)
+  end
+
 end
