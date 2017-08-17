72153b56ea47228d7cbb592820227c8bae305e24
Ted-Koomen <koomen.ted@gmail.com>
2017-08-17 10:24:44 -0400

    add editing urgency

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,6 +5,10 @@ class Job < ApplicationRecord
     self.update(title: params[:title] || self.title)
     self.update(description: params[:description] || self.description)
     self.update(date_completed: params[:date_completed] || self.date_completed)
-    self.update(urgent: params[:urgent] || self.urgent)
+    if params[:urgent] != nil
+      self.update(urgent: params[:urgent])
+    else
+      self.update(urgent: self.urgent)
+    end
   end
 end
